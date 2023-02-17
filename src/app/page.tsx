4e2e4df6910e13a8cb159400996a2ea40647c2e5
DavTheDev20 <reiddavin@outlook.com>
2023-02-16 21:49:47 -0500

    Add additional content to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import styles from './page.module.css';
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World </h1>
+      <div className={styles.jumbotron}>
+        <h1>Welcome to Black and Techy</h1>
+      </div>
+      <div className={styles['cta-section']}>
+        <h2>Be a Part of the Community!</h2>
+        <button>Join Today</button>
+      </div>
     </div>
   );
 }
